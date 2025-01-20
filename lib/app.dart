902ed2d19203cf78a984c40d9c38950dddbfb8ca
origemjhanpoll <origemjhanpoll@gmail.com>
2025-01-20 20:11:51 -0300

    adding title

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Usuário GitHub',
       debugShowCheckedModeBanner: false,
       routerDelegate: Modular.routerDelegate,
       routeInformationParser: Modular.routeInformationParser,
